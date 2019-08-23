f35863894844bf102828deeb7826c0f0731f3748
Leon <leon.id@qq.com>
2019-08-23 15:35:22 +0800

    添加项目文件。

---
+++ MailKitClient/Forms/MailClientForm.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows.Forms;
+using MailKitClient.MailKit;
+
+namespace MailKitClient.Forms
+{
+    public partial class MailClientForm : Form
+    {
+        MailKitSender mailKitSender = new MailKitSender();
+
+        MailKitReceiver mailKitReceiver = new MailKitReceiver();
+
+        public MailClientForm()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
